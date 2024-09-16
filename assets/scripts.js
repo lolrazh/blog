c7aca340f61c407871faf9443b1a5ddb2964feb3
Sandheep Rajkumar <105012644+lolrazh@users.noreply.github.com>
2024-09-16 17:12:25 +0200

    Update scripts.js

--- assets/scripts.js
+++ assets/scripts.js
@@ -2,12 +2,12 @@ document.addEventListener('DOMContentLoaded', function() {
     const cards = document.querySelectorAll('.card');
 
     cards.forEach(function(card) {
-        // Add touchstart event for mobile devices
+        // Desktop users can still use the normal hover
         card.addEventListener('touchstart', function() {
             card.classList.add('hovered');
         });
 
-        // Remove the hover effect after a short delay to simulate the hover effect
+        // Remove the hover effect after a short delay to simulate the hover effect on mobile
         card.addEventListener('touchend', function() {
             setTimeout(function() {
                 card.classList.remove('hovered');
